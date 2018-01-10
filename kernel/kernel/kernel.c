cee0e6ce7bfea24e0d93c0a60ee6ba6d87208f81
Unknown <joe.zlonicky@gmail.com>
2018-01-09 20:40:01 -0800

    Updated title

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -33,7 +33,10 @@ printf("88 `8b     d8' 88  88          88  88aaaaa        88          88  `Y8aaa
 printf("88  `8b   d8'  88  88          88  88'''''        88          88    `'''''8b,\n");  
 printf("88   `8b d8'   88  Y8,        ,8P  88             Y8,        ,8P          `8b\n");  
 printf("88    `888'    88   Y8a.    .a8P   88              Y8a.    .a8P   Y8a     a8P\n");  
-printf("88     `8'     88    `Y8888Y''     88888888888      `'Y8888Y''     'Y88888P' \n");  
-  get_char();
-  terminal_clear();
+printf("88     `8'     88    `Y8888Y''     88888888888      `'Y8888Y''     'Y88888P' \n");
+printf("88888888888888888888888888888888888888888888888888888888888888888888888888888\n");
+printf("                        8  PRESS ENTER TO CONTINUE  8                        \n");
+printf("                        88888888888888888888888888888                        \n");
+get_char();
+terminal_clear();
 }
