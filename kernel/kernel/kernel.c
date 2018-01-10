0e10014f670e9d3cfb97399c5c71df9f6dd8ef6a
Unknown <joe.zlonicky@gmail.com>
2018-01-09 20:33:52 -0800

    New title

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -26,12 +26,14 @@ void print_option(char* name, bool highlighted) {
 }
     
 void welcome_screen() {
-  printf(">M   M MMMMM MMMMM   MMMMM MMMMM\n");
-  printf("MM MM M   M M       M   M M    \n");
-  printf("M M M M   M MMM     M   M MMMMM  \n");
-  printf("M   M M   M M       M   M     M\n");
-  printf("M   M MMMMM MMMMM   MMMMM MMMMM\n");
-  printf("    PRESS ANYTHING TO START    \n");
+printf(">88b           d88    ,ad8888ba,    88888888888      ,ad8888ba,     ad88888ba\n");  
+printf("888b         d888   d8''    `'8b   88              d8''    `'8b   d8'     '8b\n");  
+printf("88`8b       d8'88  d8'        `8b  88             d8'        `8b  Y8,        \n");  
+printf("88 `8b     d8' 88  88          88  88aaaaa        88          88  `Y8aaaaa,  \n");  
+printf("88  `8b   d8'  88  88          88  88'''''        88          88    `'''''8b,\n");  
+printf("88   `8b d8'   88  Y8,        ,8P  88             Y8,        ,8P          `8b\n");  
+printf("88    `888'    88   Y8a.    .a8P   88              Y8a.    .a8P   Y8a     a8P\n");  
+printf("88     `8'     88    `Y8888Y''     88888888888      `'Y8888Y''     'Y88888P' \n");  
   get_char();
   terminal_clear();
 }
