c364a2ece56ae6673bad709b42dc627f9517d470
Unknown <joe.zlonicky@gmail.com>
2018-01-09 20:01:32 -0800

    Option printing

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #include <kernel/tty.h>
 #include <kernel/keyboard.h>
+#include <../arch/i386/vga.h>
 
 void kernel_main(void) {
   terminal_initialize();
-  printf(">");
+  print_option("Option 1", false);
+  print_option("Option 2", true);
+  print_option("Option 3", false);
   while (1) {
     printf(get_char());
   }
 }
+
+void print_option(char* name, bool highlighted) {
+  if(highlighted)
+    terminal_set_color(VGA_COLOR_BLACK | VGA_COLOR_LIGHT_GREY << 4);
+  printf("%s\n", name);
+  if(highlighted)
+    terminal_set_color(VGA_COLOR_LIGHT_GREY | VGA_COLOR_BLACK << 4);
+}
+    
+    
