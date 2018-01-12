12f0e087f5dc130196e8d1938333b1731b1f2946
Unknown <joe.zlonicky@gmail.com>
2018-01-11 19:22:18 -0800

    Added features to color menu

--- kernel/arch/i386/color_menu.c
+++ kernel/arch/i386/color_menu.c
@@ -7,52 +7,63 @@
 #include <kernel/tty.h>
 
 char first_option_list[20][20] = {"Black     ", "Blue      ", "Green     ",
-                              "Cyan      ", "Red       ", "Magenta   ",
-                              "Brown     ", "Light Grey", "Return    "};
-char second_option_list[20][20] = {"Dark grey", "Light blue", "Light green",
-                               "Light cyan", "Light red", "Light magenta",
-                               "Light brown", "White", "Apply"};
+                                  "Cyan      ", "Red       ", "Magenta   ",
+                                  "Brown     ", "Light Grey", "Return    "};
+char second_option_list[20][20] = {"Dark Grey    ", "Light Blue   ", "Light Green  ",
+                                   "Light Cyan   ", "Light Red    ", "Light Magenta",
+                                   "Yellow       ", "White        "};
 
 void color_menu_loop(char fg_or_bg) {
   int y_position = 0;
   int x_position = 0;
-  update_options(x_position, y_position);
+  update_color_menu_options(x_position, y_position);
   while(1) {
     char user_input = get_char();
-    if(user_input == '8' && y_position > 0)
+    if((user_input == 'w') && y_position > 0)
       --y_position;
-    else if(user_input == '2' && y_position < 8)
+    else if(user_input == 's' && y_position < 8) {
       ++y_position;
-    else if(user_input == '4' && x_position == 1)
+      if(x_position == 1 && y_position == 8)
+        --x_position;
+    }
+    else if(user_input == 'a' && x_position == 1)
       --x_position;
-    else if(user_input == '6' && x_position == 0)
+    else if(user_input == 'd' && x_position == 0 && y_position < 8)
       ++x_position;
-    if(user_input == '8' || user_input == '2' || user_input == '4' ||
-       user_input == '6') {
-       update_options(x_position, y_position);
+    if(user_input == 'w' || user_input == 's' || user_input == 'a' ||
+       user_input == 'd') {
+       update_color_menu_options(x_position, y_position);
     }
     if(user_input == '\n') {
       int index = y_position;
       if(x_position == 1)
         index += 8;
-      if(fg_or_bg == 'b')
-        terminal_set_bg_color(index);
-      else
-        terminal_set_fg_color(index);
-      update_options(x_position, y_position);
+      if(index == 8) {
+        terminal_clear();
+        break;
+      }
+      else {
+        if(fg_or_bg == 'b')
+          terminal_set_bg_color(index);
+        else
+          terminal_set_fg_color(index);
+        update_color_menu_options(x_position, y_position);
+      }
 
     }
   }
 }
 
-void update_options(int x_pos, int y_pos) {
+void update_color_menu_options(int x_pos, int y_pos) {
   terminal_clear();
+  printf(" Choose a color: \n");
   for(int y=0; y<9; ++y) {
     for(int x=0; x<2; ++x) {
       bool highlighted = false;
       if(x==x_pos && y==y_pos)
         highlighted = true;
       if(x==0) {
+        printf(" ");
         print_option(first_option_list[y], highlighted);
         printf("   ");
       }
@@ -60,6 +71,8 @@ void update_options(int x_pos, int y_pos) {
         print_option(second_option_list[y], highlighted);
     }
     printf("\n");
+    if(y==7)
+      printf("\n");
   }
 }
 
--- kernel/include/kernel/color_menu.h
+++ kernel/include/kernel/color_menu.h
@@ -1,6 +1,10 @@
 #ifndef _KERNEL_COLOR_MENU_H
 #define _KERNEL_COLOR_MENU_H
 
+#include <stdint.h>
+#include <stdbool.h>
 
 void color_menu_loop(char fg_or_bg);
+void update_color_menu_options(int x_pos, int y_pos);
+void print_option(char* name, bool highlighted);
 #endif
